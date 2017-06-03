e42253645a3842cbf64d514ec1e5bcb0d48d57af
macchann <ymatsunaga660@cc9.ne.jp>
2017-06-03 16:25:53 +0900

    アイテム名とアイテムのブランド名の選択による検索機能の実装。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,18 @@
 class ItemsController < ApplicationController
+
   def show
     @item = Item.find(params[:id])
   end
+
+  def search
+  end
+
+  def result
+    @items = Item.where('name LIKE(?)', "%#{params[:keyword]}%").where(create_params)
+  end
+
+  private
+    def create_params
+    params.require(:brand_id).permit(:brand_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
+  get 'items/search' => 'items#search'
+    get 'items/result' => 'items#result'
   resources :items, only: :show
+  resources :users, only: :show
+
 end
