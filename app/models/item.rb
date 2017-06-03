fdb57c4061623b32a65935c42bd8aa2f203aacd8
macchann <ymatsunaga660@cc9.ne.jp>
2017-06-03 19:38:08 +0900

    Item material style にアソシエーションを設定。

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,6 @@ class Item < ActiveRecord::Base
   belongs_to :brand
   has_many :images
   belongs_to :type
+  belongs_to :style
+  belongs_to :material
 end
--- app/models/material.rb
+++ app/models/material.rb
@@ -1,2 +1,3 @@
 class Material < ActiveRecord::Base
+  has_many :items
 end
