a19ae3347f61b4d97220bf9e72a1a652788cae69
macchann <ymatsunaga660@cc9.ne.jp>
2017-06-03 18:24:16 +0900

    Typeの検索の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,8 +9,10 @@ class ItemsController < ApplicationController
 
   def result
     brand = params.require(:brand_id).permit(:brand_id)
+    types = params.require(:type_id).permit(:type_id)
     @items = Item.all
     @items = @items.where(brand) unless brand[:brand_id].empty?
+    @items = @items.where(types) unless types[:type_id].empty?
     @items = @items.where('name LIKE(?)', "%#{params[:keyword]}%")
   end
 
