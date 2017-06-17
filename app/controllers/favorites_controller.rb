fdc916f2833e8a4a3d6271216ed4336e306e1feb
macchann <ymatsunaga660@cc9.ne.jp>
2017-06-17 18:30:18 +0900

    トップページにいいねのボタンを追加（１回のみ有効でエラーあり）

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,14 +2,16 @@ class FavoritesController < ApplicationController
   def create
     @favorite = Favorite.create(user_id: current_user.id, item_id: params[:item_id])
     @favorites = Favorite.where(item_id: params[:item_id])
-    @items = Item.all
+    @item = Item.find_by(id: params[:item_id])
+    binding.pry
   end
 
   def destroy
     favorite = Favorite.find_by(user_id: current_user.id, item_id: params[:item_id])
     favorite.destroy
     @favorite = Favorite.where(item_id: params[:item_id])
-    @items = Item.all
+    @item = Item.find_by(id: params[:item_id])
+    binding.pry
   end
 end
 
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,6 @@
 class TopController < ApplicationController
   def index
     @items = Item.order('id DESC').limit(20)
+    @favorites = Favorite.where(user_id: current_user)
   end
 end
